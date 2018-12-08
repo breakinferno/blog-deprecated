a0845f3fcbe60b2738c68753dd1ffe6dddde7e4d
breakinferno <1972952841@qq.com>
2018-12-08 23:00:32 +0800

    ready to start

--- client/vue-blog/src/components/HelloWorld.vue
+++ client/vue-blog/src/components/HelloWorld.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <Button type="error">Test iview</Button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
